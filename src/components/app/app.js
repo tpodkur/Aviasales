e6899d11a9d768a567e76f740c19017463237ed1
Tatiana <t.podkur17@gmail.com>
2024-11-21 16:06:14 +0700

    make logo and columns

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,18 @@
+import './app.scss';
+import logo from '../../assets/logo.svg';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <header className="header">
+        <img src={logo} alt="logo" />
+      </header>
+      <div className="container">
+        <div className="side-bar"></div>
+        <div className="main"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
