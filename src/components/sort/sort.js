15895c416011a8548b0716713730e80cdbc3193f
Tatiana <t.podkur17@gmail.com>
2024-11-22 14:46:43 +0700

    add styles for sort bar

--- src/components/sort/sort.js
+++ src/components/sort/sort.js
@@ -5,12 +5,18 @@ import classes from './sort.module.scss';
 const Sort = () => {
   return (
     <div className={classes.sort}>
-      <input type="radio" id="cheepest" className={classes.sort__option} checked />
-      <label htmlFor="cheepest">самый дешевый</label>
-      <input type="radio" id="fastest" className={classes.sort__option} />
-      <label htmlFor="fastest">самый быстрый</label>
-      <input type="radio" id="optimal" className={classes.sort__option} />
-      <label htmlFor="optimal">оптимальный</label>
+      <label className={`${classes.sort__option} ${classes.radio}`}>
+        <input type="radio" className={classes.radio__input} checked />
+        самый дешевый
+      </label>
+      <label className={`${classes.sort__option} ${classes.radio}`}>
+        <input type="radio" className={classes.radio__input} />
+        самый быстрый
+      </label>
+      <label className={`${classes.sort__option} ${classes.radio}`}>
+        <input type="radio" className={classes.radio__input} />
+        оптимальный
+      </label>
     </div>
   );
 };
