4ebc68badcf100103a5416e7e681c8060e4f4954
Tatiana <t.podkur17@gmail.com>
2024-11-23 13:09:15 +0700

    add styles for filter block

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -3,7 +3,21 @@ import React from 'react';
 import classes from './filter.module.scss';
 
 const Filter = () => {
-  return <div className={classes.filter}></div>;
+  const arr = ['Все', 'Без пересадок', '1 пересадка', '2 пересадки', '3 пересадки'];
+  const filterOptions = arr.map((item) => (
+    <label className={classes.option} key={Math.random().toString(16).slice(2)}>
+      <input type="checkbox" className={classes.option__input} />
+      <span className={classes.option__check}></span>
+      <span className={classes.option__label}>{item}</span>
+    </label>
+  ));
+
+  return (
+    <div className={classes.filter}>
+      <p className={classes.filter__name}>количество пересадок</p>
+      {filterOptions}
+    </div>
+  );
 };
 
 export default Filter;
