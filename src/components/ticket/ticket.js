122c2a914bc3f274c90730495c4dacc73777efdd
Tatiana <t.podkur17@gmail.com>
2024-11-23 12:06:13 +0700

    add styles for ticket

--- src/components/ticket/ticket.js
+++ src/components/ticket/ticket.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import logo from './S7_logo.png';
 import classes from './ticket.module.scss';
 
 const Ticket = () => {
   return (
     <div className={classes.card}>
       <div className={classes.card__header}>
-        <div className={classes.card__price}>13400 Р</div>
-        <div className={classes.card__company}></div>
+        <div className={classes.card__price}>13 400 Р</div>
+        <img src={logo} alt="S7-logo" />
       </div>
       <div className={classes.card__details}>
         <div className={`${classes.card__column} ${classes.column}`}>
@@ -33,11 +34,11 @@ const Ticket = () => {
         <div className={`${classes.card__column} ${classes.column}`}>
           <div className={classes.column__info}>
             <p className={classes.column__field}>2 пересадки</p>
-            <p className={classes.column__value}>hkg, jnb</p>
+            <p className={`${classes.column__value} ${classes['column__value--uppercase']}`}>hkg, jnb</p>
           </div>
           <div className={classes.column__info}>
             <p className={classes.column__field}>1 пересадка</p>
-            <p className={classes.column__value}>hkg</p>
+            <p className={`${classes.column__value} ${classes['column__value--uppercase']}`}>hkg</p>
           </div>
         </div>
       </div>
