f861e732011eeef36f2e3c52139ff38fdc84a3ff
Tatiana <t.podkur17@gmail.com>
2024-12-13 13:28:04 +0700

    implement sort

--- src/components/tickets-list/tickets-list.js
+++ src/components/tickets-list/tickets-list.js
@@ -1,15 +1,39 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
 
-import Ticket from '../ticket/ticket';
+import { CHEAP, FAST } from '../../constants/sort-status';
 import { getTickets } from '../../redux/thunks';
+import Ticket from '../ticket/ticket';
 
 import classes from './tickets-list.module.scss';
 
+const selectSortedTickets = createSelector(
+  (state) => state.tickets.ids,
+  (state) => state.tickets.entities,
+  (state) => state.sortStatus,
+  (ids, entities, sortStatus) => {
+    return ids
+      .map((id) => entities[id])
+      .sort((a, b) => {
+        if (sortStatus === CHEAP) {
+          return a.price - b.price;
+        }
+
+        const aDuration = a.segments.reduce((acc, segment) => acc + segment.duration, 0);
+        const bDuration = b.segments.reduce((acc, segment) => acc + segment.duration, 0);
+
+        if (sortStatus === FAST) {
+          return aDuration - bDuration;
+        }
+        return aDuration + a.price - (bDuration + b.price);
+      });
+  }
+);
+
 const TicketsList = () => {
   const dispatch = useDispatch();
-  const ticketsEntities = useSelector((state) => state.tickets.entities);
-  const ids = useSelector((state) => state.tickets.ids);
+  const tickets = useSelector(selectSortedTickets);
 
   useEffect(() => {
     dispatch(getTickets());
@@ -17,14 +41,11 @@ const TicketsList = () => {
 
   return (
     <ul className={classes.list}>
-      {ids
-        .map((id) => ticketsEntities[id])
-        .sort((a, b) => a.price - b.price)
-        .map((ticket) => (
-          <li className={classes.list__item} key={ticket.id}>
-            <Ticket price={ticket.price} carrier={ticket.carrier} segments={ticket.segments} />
-          </li>
-        ))}
+      {tickets.map((ticket) => (
+        <li className={classes.list__item} key={ticket.id}>
+          <Ticket price={ticket.price} carrier={ticket.carrier} segments={ticket.segments} />
+        </li>
+      ))}
     </ul>
   );
 };
